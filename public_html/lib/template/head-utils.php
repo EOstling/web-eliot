97165a569cc07c7d27e1ccf28b4e5d97bfaea1ff
Eliot Ostling <it.treugott@gmail.com>
2016-05-31 13:02:40 -0600

    adding relative path to all files

--- public_html/lib/template/head-utils.php
+++ public_html/lib/template/head-utils.php
@@ -4,9 +4,7 @@
  * @see https://raw.githubusercontent.com/kingscreations/farm-to-you/master/php/lib/_header.php FarmToYou Header
  **/
 
-// include the appropriate number of dirname() functions
-// on line 8 to correctly resolve your directory's path
-require_once(dirname(dirname(__DIR__)) . "/root-path.php");
+require_once(dirname(dirname(__DIR__)) . "../root-path.php");
 $CURRENT_DEPTH = substr_count($CURRENT_DIR, "/");
 $ROOT_DEPTH = substr_count($ROOT_PATH, "/");
 $DEPTH_DIFFERENCE = $CURRENT_DEPTH - $ROOT_DEPTH;
@@ -18,7 +16,7 @@ $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 		<meta charset="utf-8">
 		<title><?php echo $PAGE_TITLE ?></title>
 		<link data-require="bootstrap-css@3.3.6" data-semver="3.3.6" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.css" />
-		<link rel="stylesheet" href="<?php echo $PREFIX; ?>css/style.css" />
+		<link rel="stylesheet" href="<?php echo $PREFIX; ?> css/styles.css" type="text/css" />
 		<script data-require="jquery@*" data-semver="2.2.0" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<script data-require="bootstrap.js@*" data-semver="3.3.6" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<script src="script.js"></script>
